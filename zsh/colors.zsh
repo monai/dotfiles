0a2cc9787343bd92e4c41ce521148d72f4391622
monai <monai@cure.lt>
2013-11-03 19:30:46 +0200

    color variables already exists

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -3,9 +3,6 @@ autoload colors; colors
 # The variables are wrapped in %{%}. This should be the case for every
 # variable that does not contain space.
 for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
-    eval COLOR_$COLOR='$fg_no_bold[${(L)COLOR}]'
-    eval COLOR_BOLD_$COLOR='$fg_bold[${(L)COLOR}]'
-    
     eval PR_$COLOR='%{$fg_no_bold[${(L)COLOR}]%}'
     eval PR_BOLD_$COLOR='%{$fg_bold[${(L)COLOR}]%}'
 done
