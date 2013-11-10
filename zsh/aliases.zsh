fd72d4945d0a4b158a4ed6fa0c4d53713dc49b8c
monai <monai@cure.lt>
2013-11-10 11:54:25 +0200

    no longer needed

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -40,17 +40,6 @@ alias gra='git remote add'
 alias grr='git remote rm'
 alias gpu='git pull'
 alias gcl='git clone'
-alias gta='git tag -a -m'
+alias gta='git tag -am'
 alias gf='git reflog'
 alias gv='git log --pretty=format:'%s' | cut -d " " -f 1 | sort | uniq -c | sort -nr'
-
-# leverage aliases from ~/.gitconfig
-alias gh='git hist'
-alias gt='git today'
-
-# curiosities 
-# gsh shows the number of commits for the current repos for all developers
-alias gsh="git shortlog | grep -E '^[ ]+\w+' | wc -l"
-
-# gu shows a list of all developers and the number of commits they've made
-alias gu="git shortlog | grep -E '^[^ ]'"
--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -11,7 +11,6 @@ zstyle ':completion::complete:*' use-cache on
 zstyle ':completion::complete:*' cache-path ~/.zsh/cache/$HOST
 
 # Fallback to built in ls colors
-#zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 # Make the list prompt friendly
