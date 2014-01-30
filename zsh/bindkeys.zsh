f80c8d8fce713f1502e406bb9af1d770b5289102
monai <monai@cure.lt>
2014-01-30 22:43:31 +0200

    updated key bindings

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -2,13 +2,26 @@
 # cat > /dev/null
 # And press it
 
-bindkey "^K"      kill-whole-line                      # ctrl-k
-bindkey "^R"      history-incremental-search-backward  # ctrl-r
-bindkey "^A"      beginning-of-line                    # ctrl-a  
-bindkey "^E"      end-of-line                          # ctrl-e
-bindkey "[B"      history-search-forward               # down arrow
-bindkey "[A"      history-search-backward              # up arrow
-bindkey "^D"      delete-char                          # ctrl-d
-bindkey "^F"      forward-char                         # ctrl-f
-bindkey "^B"      backward-char                        # ctrl-b
 bindkey -v   # Default to standard vi bindings, regardless of editor string
+
+bindkey "^[^[[D"    backward-word   # alt + <-
+bindkey "^[[1;5D"   backward-word
+bindkey "^[^[[C"    forward-word    # alt + ->
+bindkey "^[[1;5C"   forward-word
+
+bindkey "^[[5~"     up-line-or-history
+bindkey "^[[6~"     down-line-or-history
+
+bindkey "^[[H"      beginning-of-line
+bindkey "^[[1~"     beginning-of-line
+bindkey "^[OH"      beginning-of-line
+bindkey "^[[F"      end-of-line
+bindkey "^[[4~"     end-of-line
+bindkey "^[OF"      end-of-line
+bindkey " "         magic-space
+
+bindkey "^?"        backward-delete-char
+bindkey "^[[3~"     delete-char
+bindkey "^[3;5~"    delete-char
+bindkey "\e[3~"     delete-char
+
