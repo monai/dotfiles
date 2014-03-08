a61f6a2aa87e37707136e84ace39442ae682e493
monai <monai@cure.lt>
2014-03-08 15:53:00 +0200

    updated exports

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -1,14 +1,17 @@
 autoload colors;
 colors
 
-# The variables are wrapped in %{%}. This should be the case for every
-# variable that does not contain space.
+# Color variables for prompt.
+# The variables are wrapped in %{%}. This should be the case for every variable that does not contain space.
 for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
     eval PR_$COLOR='%{$fg_no_bold[${(L)COLOR}]%}'
     eval PR_BOLD_$COLOR='%{$fg_bold[${(L)COLOR}]%}'
 done
+eval PR_RESET='$reset_color'
 
-export TERM=xterm-256color
-export CLICOLOR=1
-export LSCOLORS=exfxcxdxbxegedabagacad
-export LS_COLORS='di=34;50:ln=35;50:so=32;50:pi=33;50:ex=31;50:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+CLICOLOR=1
+LSCOLORS=exfxcxdxbxegedabagacad
+LS_COLORS='di=34;50:ln=35;50:so=32;50:pi=33;50:ex=31;50:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+
+GREP_OPTIONS='--color=auto'
+GREP_COLOR='3;33'
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,10 +1,14 @@
-# Enable color in grep
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='3;33'
+export CLICOLOR
+export LSCOLORS
+export LS_COLORS
+
+export GREP_OPTIONS
+export GREP_COLOR
 
 # This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions
 export ARCHFLAGS='-arch x86_64'
 
+export TERM=xterm-256color
 export LESS='--ignore-case --raw-control-chars'
 export PAGER='less'
 export EDITOR='vim'
