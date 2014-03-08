7b5202c87873650f11556ca680d83b58966b6738
monai <monai@cure.lt>
2014-03-08 19:33:27 +0200

    updated completion on mac

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -53,3 +53,9 @@ zstyle ':completion:*:scp:*' group-order files all-files users hosts-domain host
 zstyle ':completion:*:ssh:*' tag-order users 'hosts:-host hosts:-domain:domain hosts:-ipaddr"IP\ Address *'
 zstyle ':completion:*:ssh:*' group-order hosts-domain hosts-host users hosts-ipaddr
 zstyle '*' single-ignored show
+
+if [ HAS_BREW ]; then
+    BREW_PATH="$(brew --prefix)"
+    fpath=("${BREW_PATH}/share/zsh-completions" $fpath)
+    source "${BREW_PATH}/etc/bash_completion.d/*"
+fi
