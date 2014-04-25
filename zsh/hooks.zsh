cff5843ae115c3fb27e69db4e3503de48be964fa
monai <monai@cure.lt>
2014-04-25 15:24:13 +0300

    added vcs_info to hook

--- zsh/hooks.zsh
+++ zsh/hooks.zsh
@@ -1,10 +1,14 @@
+autoload -Uz vcs_info
+
 function precmd() {
-    # vcs_info
-    # Put the string "hostname::/full/directory/path" in the title bar:
+    # Put the string "hostname::/full/directory/path" in the title bar
     echo -ne "\e]2;$PWD\a"
     
     # Put the parentdir/currentdir in the tab
     echo -ne "\e]1;$PWD:h:t/$PWD:t\a"
+    
+    # Get the current git branch into the prompt
+    vcs_info
 }
 
 function preexec() {
