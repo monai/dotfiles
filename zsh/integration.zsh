8f080e0d82a82558a2c6c649a0c803673564ed07
monai <monai@cure.lt>
2014-04-25 17:05:18 +0300

    added support for virtualenvwrapper on ubuntu

--- zsh/integration.zsh
+++ zsh/integration.zsh
@@ -1,4 +1,11 @@
-# pyenv
+# python
 if which pyenv > /dev/null; then
     eval "$(pyenv init -)";
+else
+    vw=/usr/share/virtualenvwrapper/virtualenvwrapper.sh
+    if [ -f $vw ]; then
+        source $vw
+    fi
 fi
+
+
