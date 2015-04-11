b11d250db3440746e62e5716e226fb34fc6e1a46
monai <monai@cure.lt>
2015-04-11 15:39:04 +0300

    simplified functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,7 +1,7 @@
 # Extract archive
 function ex() {
     readonly name=$(basename $0)
-    local file=$1
+    readonly file=$1
     
     if $(is_empty $file); then
         echo "usage: $name archive" >&2
@@ -32,34 +32,36 @@ function ex() {
 # Process pattern search
 function ps-grep() {
     readonly name=$(basename $0)
-    local pattern=$1
+    readonly pattern=$1
     
-    emulate -L zsh
-    unsetopt KSH_ARRAYS
     if $(is_empty $pattern); then
         echo "usage: $name pattern" >&2
         return 1
     else
-        ps xauwww | grep -i --color=auto "[${pattern[1]}]${pattern[2,-1]}"
+        ps aux \
+            | grep "$pattern"
     fi
 }
 
 # Display a neatly formatted path
 function path() {
-    echo $PATH | tr ":" "\n" | \
-        awk "{ sub(\"/usr\",   \"$FG[green]/usr$FX[reset]\"); \
-               sub(\"/bin\",   \"$FG[blue]/bin$FX[reset]\"); \
-               sub(\"/opt\",   \"$FG[cyan]/opt$FX[reset]\"); \
-               sub(\"/sbin\",  \"$FG[magenta]/sbin$FX[reset]\"); \
-               sub(\"/local\", \"$FG[yellow]/local$FX[reset]\"); \
-               print }"
+    echo $PATH \
+        | tr ":" "\n" \
+        | awk "{ sub(\"/usr\",   \"$FG[green]/usr$FX[reset]\"); \
+                 sub(\"/bin\",   \"$FG[blue]/bin$FX[reset]\"); \
+                 sub(\"/opt\",   \"$FG[cyan]/opt$FX[reset]\"); \
+                 sub(\"/sbin\",  \"$FG[magenta]/sbin$FX[reset]\"); \
+                 sub(\"/local\", \"$FG[yellow]/local$FX[reset]\"); \
+                 print }"
 }
 
 # Tail syslog
 function syslog-tail() {
-    if [[ $# -gt 0 ]]; then
-        query=$(echo "$*" | tr -s ' ' '|')
-        tail -f /var/log/system.log | grep -i --color=auto -E "$query"
+    set -A patterns $@
+    readonly query=${(j:|:)patterns}
+    
+    if $(is_not_empty $patterns); then
+        tail -f /var/log/system.log | grep -E "$query"
     else
         tail -f /var/log/system.log
     fi
