95888bee4c424f7986dadac5075c73a253604363
monai <monai@cure.lt>
2017-07-26 12:48:17 +0300

    Improve completion

--- myzsh/modules/bootstrap/init.zsh
+++ myzsh/modules/bootstrap/init.zsh
@@ -3,10 +3,51 @@ bootstrap_load_module() {
   zgen prezto "${module}"
 }
 
+atom() {
+  local args="$@"
+  if (( $+commands[atom-beta] )); then
+    $commands[atom-beta] ${=args}
+  else
+    $commands[atom] ${=args}
+  fi
+}
+
+aws() {
+  local args="$@"
+  unset -f aws
+  load_zsh_completion "_aws"
+  aws ${=args}
+}
+
+dnvm() {
+  local args=${1+"$@"}
+  local init=dnvm.sh
+
+  unset -f dnvm
+
+  if [[ -x $(which "${init}" 2>/dev/null) ]]; then
+    source "${init}"
+    export DOTNET_CLI_TELEMETRY_OPTOUT=1
+    if is_empty "${args}"; then
+      dnvm
+    else
+      dnvm ${=args}
+    fi
+  fi
+}
+
+kubectl() {
+  local args="$@"
+  unset -f kubectl
+  load_zsh_completion "_kubectl"
+  kubectl ${=args}
+}
+
 nvm() {
   local args="$@"
   unset -f nvm
   bootstrap_load_module node
+  load_bash_completion "nvm"
   nvm ${=args}
 }
 
@@ -36,21 +77,11 @@ rbenv() {
   rbenv ${=args}
 }
 
-dnvm() {
-  local args=${1+"$@"}
-  local init=dnvm.sh
-
-  unset -f dnvm
-
-  if [[ -x $(which "${init}" 2>/dev/null) ]]; then
-    source "${init}"
-    export DOTNET_CLI_TELEMETRY_OPTOUT=1
-    if is_empty "${args}"; then
-      dnvm
-    else
-      dnvm ${=args}
-    fi
-  fi
+rustup() {
+  local args="$@"
+  unset -f rustup
+  source $HOME/.cargo/env
+  rustup ${=args}
 }
 
 stack() {
@@ -59,20 +90,4 @@ stack() {
   autoload -U +X bashcompinit && bashcompinit
   eval "$(stack --bash-completion-script stack)"
   stack ${=args}
-}
-
-rustup() {
-  local args="$@"
-  unset -f rustup
-  source $HOME/.cargo/env
-  rustup ${=args}
-}
-
-atom() {
-  local args="$@"
-  if (( $+commands[atom-beta] )); then
-    $commands[atom-beta] ${=args}
-  else
-    $commands[atom] ${=args}
-  fi
 }
---
+++ myzsh/modules/completion/init.zsh
@@ -0,0 +1,18 @@
+load_zsh_completion() {
+  local comp_path="/usr/local/share/zsh/site-functions"
+  local filename="${1}"
+  load_completion "${comp_path}/${filename}"
+}
+
+load_bash_completion() {
+  local comp_path="/usr/local/etc/bash_completion.d"
+  local filename="${1}"
+  load_completion "${comp_path}/${filename}"
+}
+
+load_completion() {
+  local filename=$1
+  if [[ -f "${filename}" ]]; then
+    source "${filename}"
+  fi
+}
