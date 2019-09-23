f12bd687f89a74f5a7ebd146ed9bbdc9380e0a0a
monai <monai@cure.lt>
2019-09-23 09:44:03 +0300

    Add freespace command

---
+++ myzsh/modules/my-utils/init.zsh
@@ -0,0 +1,14 @@
+freespace() {
+  sudo rm -rf /.DocumentRevisions-V100
+  rm -rf "$HOME/Library/Developer/Xcode/DerivedData"
+  rm -rf "$HOME/Library/Developer/Xcode/Archives"
+  rm -rf "$HOME/Library/Developer/Xcode/iOS\ DeviceSupport"
+  rm -rf "$HOME/Library/Caches/com.apple.dt.Xcode"
+  xcrun simctl delete unavailable
+}
+
+route-vpn() {
+  sudo route add -net 10.0.0.0/8 -interface ppp0
+  sudo route add -net 172.16.0.0/12 -interface ppp0
+  sudo route add -net 192.168.0.0/16 -interface ppp0
+}
--- myzsh/modules/my-vpn/init.zsh
+++
@@ -1,5 +0,0 @@
-work-vpn() {
-  sudo route add -net 10.0.0.0/8 -interface ppp0
-  sudo route add -net 172.16.0.0/12 -interface ppp0
-  sudo route add -net 192.168.0.0/16 -interface ppp0
-}
