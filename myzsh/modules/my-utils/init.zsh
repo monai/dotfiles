6f215ba13994e15f689f276d5572a4349e3ebefe
monai <monai@cure.lt>
2019-12-23 22:34:06 +0200

    Add function clear-completion-cache

--- myzsh/modules/my-utils/init.zsh
+++ myzsh/modules/my-utils/init.zsh
@@ -12,3 +12,9 @@ route-vpn() {
   sudo route add -net 172.16.0.0/12 -interface ppp0
   sudo route add -net 192.168.0.0/16 -interface ppp0
 }
+
+clear-completion-cache() {
+  rm -rf "$HOME/.zcompcache"
+  rm -rf "$HOME/.zcompdump"
+  rm -rf "$HOME/.zcompdump.zwc"
+}
