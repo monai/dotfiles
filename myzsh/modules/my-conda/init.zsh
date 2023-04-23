c0ac24c7e83938639563df8f72797f66e45388c8
monai <monai@cure.lt>
2023-04-23 10:45:52 +0300

    Extract CONDA_PREFIX

--- myzsh/modules/my-conda/init.zsh
+++ myzsh/modules/my-conda/init.zsh
@@ -1,13 +1,15 @@
+CONDA_PREFIX="${BREW_PREFIX}/anaconda3"
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('${BREW_PREFIX}/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('${CONDA_PREFIX}/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
   eval "$__conda_setup"
 else
-  if [ -f "${BREW_PREFIX}/anaconda3/etc/profile.d/conda.sh" ]; then
-    . "${BREW_PREFIX}/anaconda3/etc/profile.d/conda.sh"
+  if [ -f "${CONDA_PREFIX}/etc/profile.d/conda.sh" ]; then
+    . "${CONDA_PREFIX}/etc/profile.d/conda.sh"
   else
-    export PATH="${BREW_PREFIX}/anaconda3/bin:$PATH"
+    export PATH="${CONDA_PREFIX}/bin:$PATH"
   fi
 fi
 unset __conda_setup
